45ab6cada1e07db74668ecd491f08a4b65538921
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-06-27 13:23:15 +0100

    Add doctor signIn

---
+++ src/components/SignIn.js
@@ -0,0 +1,26 @@
+import DoctorForm from "./DoctorForm";
+
+const SignIn = () => {
+    return ( 
+        <div className="container">
+            <nav className="brand-name">
+                <h1><span className="white-line">Me</span>dli<span className="red-line">nk</span></h1>
+            </nav>
+            <div className="content">
+                <h2>Sign In</h2>
+                <DoctorForm />
+            </div>
+            <footer className="med-personnels">
+                <ul className="nav-footer">
+                    <li className="none"><a href="">Admin</a></li>
+                    <li><a>Pharm</a></li>
+                    <li><a>Lab</a></li>
+                    <li className="active"><a>Doctor</a></li>
+                </ul>
+                <button type="submit">Login</button>
+            </footer>
+        </div>
+     );
+}
+ 
+export default SignIn;
