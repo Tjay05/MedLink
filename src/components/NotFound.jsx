c7e0530e11524c505385b090ebbaef0c3a0a996b
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-07-30 20:53:06 +0100

    Begin commit streak

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
     return ( 
         <div className="unavailable">
             <h2>Page not Found!!!</h2>
-            <p>You have appeared to visit a page that doesn't exist in this domain. To return back to the homepage <Link to="/home/">click here</Link>.</p>
+            <p>You have appeared to visit a page that doesn't exist in this domain. To return back to the homepage <Link to="/">click here</Link>.</p>
         </div>
      );
 }
