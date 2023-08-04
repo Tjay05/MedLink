76f921699c6b121431730c8037df582f648914cb
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-08-04 11:47:26 +0100

    Add profile pic popup

--- src/components/dochome/DocProfile.jsx
+++ src/components/dochome/DocProfile.jsx
@@ -3,9 +3,11 @@ import logoutbtn from "../../assets/icons/logout.svg";
 import { useNavigate } from "react-router-dom";
 import { TbCameraPlus } from 'react-icons/tb';
 import whiteAvatar from "../../assets/icons/Avatar1.svg"
+import { GrClose } from "react-icons/gr";
 
 const DocProfile = () => {
     const history = useNavigate();
+    const [isClosed, setIsClosed] = useState(false);
     const [isPending, setIsPending] = useState(false);
     const [isUploading, setIsUploading] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
@@ -34,6 +36,11 @@ const DocProfile = () => {
           });
       }, []);
 
+    const handleExit = () => {
+        setIsClosed(true);
+        
+    };
+
     const handleUpload = async() => {
         setIsUploading(true);
         try {
@@ -48,6 +55,8 @@ const DocProfile = () => {
             if(response.ok){
                 console.log(data);
                 setIsUploading(false);
+                setIsClosed(true);
+                setPic(URL.createObjectURL(avatar));
             }else{
                 console.log('Image upload failed',data);
                 setIsUploading(false);
@@ -76,6 +85,8 @@ const DocProfile = () => {
                 setIsPending(false)
                 localStorage.removeItem('doctor');
                 history('/');
+            } else {
+                setIsPending(false)
             }
         } catch(err) {
             console.log(err);
@@ -84,6 +95,18 @@ const DocProfile = () => {
 
     return ( 
         <div className="profileWrap">
+            {avatar && !isClosed && (
+                      <div className='av__upload'>
+                        <div className="sideNav">
+                        <div className="closeIcon"><GrClose onClick={handleExit} className='closeIcon__icon ' /></div>
+                            <div className="sideNavProf">
+                                <img src={URL.createObjectURL(avatar)} alt="Selected" />
+                                {!isUploading && <button onClick={handleUpload} >Update</button>}
+                                {isUploading && <button>Updating Display pic...</button>}
+                            </div>
+                        </div>
+                      </div>
+                    )}
             <div className="profilePic">
                 <img className="PiC" src={pic === null ? whiteAvatar : pic }/>
                 <div className="ndPic">
@@ -93,14 +116,7 @@ const DocProfile = () => {
                         accept="image/png,/image/jpeg,/image/jpg" 
                         id="upload" 
                     />
-                    {avatar && (
-                      <div className='av__upload'>
-                        <img src={URL.createObjectURL(avatar)} alt="Selected" />
-                        {!isUploading && <button onClick={handleUpload} >Upload</button>}
-                        {isUploading && <button>Uploading Display pic...</button>}
-                      </div>
-                    )}
-                    <label htmlFor="upload">{!avatar &&  <TbCameraPlus className="cam" />}</label> 
+                    <label htmlFor="upload">{ <TbCameraPlus className="cam" />}</label> 
                 </div>
             </div>
             <div className="profileHead">
